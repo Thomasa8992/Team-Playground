32dd7ebbdc872da2c6469556e94996d1162b5ddb
Adrian Thomas <thomasa8992@hotmail.com>
2020-08-15 10:14:09 -0500

    Added assets, basic walking, animation and player movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float cameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObject;
+    Vector3 followPosition;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject cameraObject;
+    public GameObject playerObject;
+    public float cameraDistanceXToPlayer;
+    public float cameraDistanceYToPlayer;
+    public float cameraDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    public float rotationX = 0.0f;
+    public float rotationY = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotationX = rot.x;
+        rotationY = rot.y;
+
+        //locks the cursor in place in the game
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //setup the rotation of the sticks
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotationY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotationX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotationX = Mathf.Clamp(rotationX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotationX, rotationY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate() {
+        CameraUpdater();
+    }
+
+    void CameraUpdater() {
+        Transform target = cameraFollowObject.transform;
+
+        float step = cameraMoveSpeed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float movementSpeed = 6;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        anim = GameObject.Find("GFX").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //normalizing this vector will insure that a consistent speed is used when the character is moving in all directions
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f) {
+            anim.SetBool("walking", true);
+
+            //calculate the turn direction based on camera direction
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            //smooths the angles for character turning to keep from having hard snap turning
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(moveDirection.normalized * movementSpeed * Time.deltaTime);
+        } else {
+            anim.SetBool("walking", false);
+        }
+    }
+}
